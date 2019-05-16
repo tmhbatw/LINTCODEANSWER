13c0fae5972ca36f3cdf35bed8208dc22d6a5561
tmhbatw <1060226998@qq.com>
2019-05-16 19:20:01 +0800

    LINTCODE answer 更新了LINTCODE第一题的解答

--- src/LINTCODE/LINTCODE1.java
+++ src/LINTCODE/LINTCODE1.java
@@ -1,6 +1,18 @@
 package LINTCODE;
 
 public class LINTCODE1 {
+    /*
+    Description
+        Write a function that add two numbers A and B.
+    * */
+
+
+    /*当然可以使用加法运算符返回题目答案
+      亦可以使用按位运算符号
+      a^b得出结果即为a与b两整数无进位结果
+      a&b<<1得出结果为a与b两整数进位结果
+      二者相加知道没有进位即为运算结果
+    * */
     public static  int aplusb(int a, int b) {
         while((a&b)<<1!=0){
             int temp=a^b;
@@ -8,8 +20,5 @@ public class LINTCODE1 {
             a=temp;
         }
         return a^b;
-    }
-    public static void main(String[] args){
-        System.out.println(aplusb(3,6));
     }
 }
