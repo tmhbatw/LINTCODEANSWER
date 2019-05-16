c3d79f7bc2e7e3505dc52627a7f45a6220de51e3
tmhbatw <1060226998@qq.com>
2019-05-16 19:41:24 +0800

    LINTCODE answer 更新了LINTCODE第一题的解答

--- src/LINTCODE/LINTCODE3.java
+++ src/LINTCODE/LINTCODE3.java
@@ -1,6 +1,13 @@
 package LINTCODE;
 
 public class LINTCODE3 {
+    /*Description
+    Count the number of k's between 0 and n. k can be 0 - 9.
+    * */
+
+    /*Solution
+    暴力遍历每一个数字可以获得答案
+    * */
     public int digitCounts(int k, int n) {
         int result=0;
         char temp=String.valueOf(k).charAt(0);
