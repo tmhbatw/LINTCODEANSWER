6d84a7c7fca5c9b6b6e8fe254eac9a849ec7c486
tmhbatw <1060226998@qq.com>
2019-05-16 20:41:26 +0800

    LINTCODE answer 更新了LINTCODE第五题的解答

--- src/LINTCODE/LINTCODE5.java
+++ src/LINTCODE/LINTCODE5.java
@@ -4,6 +4,17 @@ package LINTCODE;
 import java.util.Arrays;
 
 public class LINTCODE5 {
+    /*Description
+    *Find K-th largest element in an array.
+    * tips:You can swap elements in the array
+    * */
+
+    /*Solution
+    *使用快速排序的方法
+    * 取数组第一个数字，将大于它的k个数字移到左边，小于它的n-1-k个数字右移，将他放在第k+1位上，则这个数字第k+1大
+    *向左或者向右递归即可求得第j大的数字
+    * （PS：LINTCODE中50002测试用例包含数字相同情况）
+    * */
     public static int kthLargestElement(int n, int[] nums) {
         return quickSort(nums,0,nums.length-1,n);
     }
@@ -12,11 +23,11 @@ public class LINTCODE5 {
         int fla=left;
         left=left+1;
         while(left<=right){
-            if(nums[right]<flag){
+            if(nums[right]<=flag){
                 right--;
                 continue;
             }
-            if(nums[left]>=flag){
+            if(nums[left]>flag){
                 left++;
                 continue;
             }
@@ -24,21 +35,17 @@ public class LINTCODE5 {
             nums[right]=nums[left];
             nums[left]=tem;
             left++;
+            right--;
         }
         nums[fla]=nums[left-1];
         nums[left-1]=flag;
-        System.out.println(Arrays.toString(nums));
-        System.out.println(left);
         if(n==left){
             return nums[left-1];
         }else if(n>left){
-            return quickSort(nums,left+1,nums.length-1,n);
+            return quickSort(nums,left,nums.length-1,n);
         }else {
-            return quickSort(nums, 0, left-1, n);
+            return quickSort(nums, fla, left-1, n);
         }
     }
-    public static void main(String[] args){
-        int[] nums={9,3,2,4,8};
-        System.out.println(kthLargestElement(3,nums));
-    }
+
 }
