7f3be0d4d4c6a2a7c3d0ed9c796778a008706d56
tmhbatw <1060226998@qq.com>
2019-05-29 00:35:09 +0800

    LINTCODE answer                  更新了LINTCODE第72题的解答

---
+++ src/LINTCODE/LINTCODE72.java
@@ -0,0 +1,48 @@
+package LINTCODE;
+
+import datastructure.TreeNode;
+
+public class LINTCODE72 {
+    /*Description
+    *Given inorder and postorder traversal of a tree, construct the binary tree.
+    * */
+
+    /*Solution
+    *后序遍历的最后一个点为根节点，在中序中找到这个节点
+    * 则该节点左边为左节点，右边为右节点
+    * 进行递归即可
+    * */
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if(inorder.length==0||inorder.length!=postorder.length)
+            return null;
+        if(inorder.length==1)
+            return new TreeNode(inorder[0]);
+        return get(inorder,0,inorder.length-1,postorder,0,postorder.length-1);
+        // write your code here
+    }
+
+    private TreeNode get(int[] inorder,int instart,int inend,int[] postorder,int poststart,int postend){
+        if(instart>inend)
+            return null;
+        if(instart==inend)
+            return new TreeNode(inorder[instart]);
+        TreeNode root=new TreeNode(postorder[postend]);
+        int index=findIndex(inorder,instart,inend,postorder[postorder.length-1]);
+        root.left=get(inorder,instart,index-1,postorder,poststart,poststart+(index-1-instart));
+        System.out.println((index+1)+"   "+(inend));
+        System.out.println((postend-1-(inend-index-1))+"   "+(postend-1));
+        root.right=get(inorder,index+1,inend,postorder,postend-1-(inend-index-1),postend-1);
+        return root;
+    }
+
+
+    //用来获取根节点的坐标
+    private int findIndex(int[] inorder,int start,int end,int key){
+        for(int i=start;i<=end;i++){
+            if(inorder[i]==key)
+                return i;
+        }
+        return -1;
+    }
+}
