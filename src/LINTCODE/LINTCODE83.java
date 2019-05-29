ee826f7612e956e2d297d0402fd2c39c2c9c36a7
tmhbatw <1060226998@qq.com>
2019-05-30 01:58:12 +0800

    LINTCODE answer                  更新了LINTCODE第83、84题的解答

---
+++ src/LINTCODE/LINTCODE83.java
@@ -0,0 +1,32 @@
+package LINTCODE;
+
+public class LINTCODE83 {
+    /*Description
+    *Given 3*n + 1 non-negative integer, every numbers occurs triple times except one, find it.
+    * */
+
+    /*Solution
+    * 用一个32位数组表示一个Integer的每一位
+    * 当某个数出现3次时，某一位必然出现3次，因此每当某一位数组变为3时，清0
+    * 数组中剩下的最后的数即表示唯一出现一次的数
+    * */
+
+    public int singleNumberII(int[] A) {
+        // write your code here
+        int[] count=new int[32];
+        int result=0;
+        for(int i=0;i<A.length;i++){
+            for(int j=0;j<32;j++){
+                count[j]+=(A[i]>>j&1);
+/*                if(count[j]==3){
+                    count[j]=0;
+                }*/
+            }
+        }
+        for(int i=0;i<32;i++){
+            count[i]=count[i]%3;
+            result=result|(count[i]<<i);
+        }
+        return result;
+    }
+}
---
+++ src/LINTCODE/LINTCODE84.java
@@ -0,0 +1,45 @@
+package LINTCODE;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LINTCODE84 {
+    /*Description
+    *Given 2*n + 2 numbers, every numbers occurs twice except two, find them
+    * */
+
+    /*Solution
+    *每一个数取异或操作，则剩下的值必定为两个不重复数的亦或值
+    * 因为两个数不相同，则结果必定有一位为1
+    * 将这一位为0或1分为两组，则两个不重复数必定在不同的组中
+    * 两个组分别取异或值即可
+    * */
+
+
+    public List<Integer> singleNumberIII(int[] A) {
+        int result=0;
+        for(int i=0;i<A.length;i++){
+            result^=A[i];
+        }
+        int count=0;
+        for(int i=0;i<32;i++){
+            if((result>>i&1)==1){
+                count=i;
+                break;
+            }
+        }
+        int result1=0,result2=0;
+        for(int i=0;i<A.length;i++){
+            if((A[i]>>count&1)==1){
+                result1^=A[i];
+            }else{
+                result2^=A[i];
+            }
+        }
+        List<Integer> res=new ArrayList<>();
+        res.add(result1);
+        res.add(result2);
+        return res;
+        // write your code here
+    }
+}
