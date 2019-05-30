1c5b1ea77e50ac917598f3298ff726a3367b7330
tmhbatw <1060226998@qq.com>
2019-05-31 01:48:11 +0800

    LINTCODE answer                  更新了LINTCODE第145题的解答

---
+++ src/LINTCODE2/LINTCODE145.java
@@ -0,0 +1,15 @@
+package LINTCODE2;
+
+public class LINTCODE145 {
+    /*Description
+    *Convert a lowercase character to uppercase.
+    * */
+
+    /*Solution
+    * 没什么难点
+    * */
+    public char lowercaseToUppercase(char character) {
+        return (char) (character+'A'-'a');
+        // write your code here
+    }
+}
