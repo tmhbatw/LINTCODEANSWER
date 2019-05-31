34556e25fa71d58217da1368e30d8ccde64ed7d5
tmhbatw <1060226998@qq.com>
2019-05-31 15:31:39 +0800

    更新

--- src/datastructure/RandomListNode.java
+++ src/datastructure/RandomListNode.java
@@ -1,7 +1,7 @@
 package datastructure;
 
-  class RandomListNode {
-      int label;
-      RandomListNode next, random;
-      RandomListNode(int x) { this.label = x; }
+  public class RandomListNode {
+      public int label;
+      public RandomListNode next, random;
+      public RandomListNode(int x) { this.label = x; }
   };
