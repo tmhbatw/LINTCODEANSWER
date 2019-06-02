4fe4bf3c241c0425f14b9fd856266d6d390d0a5b
tmhbatw <1060226998@qq.com>
2019-06-02 14:03:24 +0800

    LINTCODE answer                  更新了一个数据结构

---
+++ src/datastructure/DirectedGraphNode.java
@@ -0,0 +1,12 @@
+package datastructure;
+
+import java.util.ArrayList;
+
+public class DirectedGraphNode {
+     public int label;
+      public ArrayList<DirectedGraphNode> neighbors;
+      public DirectedGraphNode(int x) {
+          label = x;
+          neighbors = new ArrayList<DirectedGraphNode>();
+      }
+  };
