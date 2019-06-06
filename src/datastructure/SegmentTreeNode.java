3f5eb90a1962529f0b3c42a925468ec89b92fdcd
tmhbatw <1060226998@qq.com>
2019-06-06 17:00:28 +0800

    LINTCODE answer                  更新了数据结构

--- src/datastructure/SegmentTreeNode.java
+++ src/datastructure/SegmentTreeNode.java
@@ -2,10 +2,18 @@ package datastructure;
 
   public class SegmentTreeNode {
       public int start, end;
+      public int max;
       public SegmentTreeNode left, right;
       public SegmentTreeNode(int start, int end) {
           this.start = start;
           this.end = end;
           this.left = this.right = null;
+      }
+
+      public SegmentTreeNode(int start,int end,int max){
+          this.start=start;
+          this.end=end;
+          this.max=max;
+          this.left=this.right=null;
       }
   }
--- src/datastructure/UndirectedGraphNode.java
+++ src/datastructure/UndirectedGraphNode.java
@@ -6,6 +6,6 @@ import java.util.ArrayList;
       public class UndirectedGraphNode {
       public int label;
       public ArrayList<UndirectedGraphNode> neighbors;
-      UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+      public UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
   };
 
