46ac024b5654d85d6dff90b1e19c56d6b8ed22b2
tmhbatw <1060226998@qq.com>
2019-06-07 16:35:52 +0800

    LINTCODE answer                  更新了一个数据结构

--- src/datastructure/SegmentTreeNode.java
+++ src/datastructure/SegmentTreeNode.java
@@ -1,6 +1,6 @@
 package datastructure;
 
-  public class SegmentTreeNode {
+/*  public class SegmentTreeNode {
       public int start, end;
       public int max;
       public SegmentTreeNode left, right;
@@ -16,4 +16,14 @@ package datastructure;
           this.max=max;
           this.left=this.right=null;
       }
-  }
+      */
+public class SegmentTreeNode {
+     public int start, end, count;
+     public SegmentTreeNode left, right;
+     public SegmentTreeNode(int start, int end, int count) {
+          this.start = start;
+         this.end = end;
+         this.count = count;
+         this.left = this.right = null;
+     }
+}
