8f7c624d70b1f61d1eb4c73b0b2b5ea3c5d06efa
tmhbatw <1060226998@qq.com>
2019-06-28 23:55:58 +0800

    LINTCODE answer                  更新了一个数据结构

---
+++ src/datastructure/Tweet.java
@@ -0,0 +1,12 @@
+package datastructure;
+
+class Tweet {
+    public int id;
+    public int user_id;
+    public String text;
+
+    public static Tweet create(int user_id, String tweet_text) {
+        return null;
+
+    }
+}
