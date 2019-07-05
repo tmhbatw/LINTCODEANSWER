beebb57d4af7ced1add8119da65bb4ec08a9e5da
tmhbatw <1060226998@qq.com>
2019-07-06 04:18:31 +0800

    LINTCODE answer                  更新了LINTCODE第222题的解答

---
+++ src/LINTCODE3/LINTCODE222.java
@@ -0,0 +1,14 @@
+package LINTCODE3;
+
+public class LINTCODE222 {
+    /*Description
+    * 创建一个getter和setter方法
+    * */
+    private String name;
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return this.name;
+    }
+}
