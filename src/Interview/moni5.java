fbd6a053ed3a8734595f14ab0b321743e21ccf60
tmhbatw <1060226998@qq.com>
2019-07-30 00:15:42 +0800

    更新了一部分模拟练习的代码

--- src/Interview/moni5.java
+++ src/Interview/moni5.java
@@ -6,21 +6,56 @@ import java.util.Scanner;
 
 public class moni5 {
 
+    /*Description
+    * 题目要求获取一个数组中连续上升的数组子序列，最多有一个数字不满足连续上升要求
+    * */
+
+    private static int getNum(int[] num){
+        int res=0;
+        int start=0;
+        int curMax=num[0];
+        int curIndex=-1;
+        for(int i=1;i<num.length;i++){
+            if(num[i]>curMax) {
+                curMax=num[i];
+                continue;
+            }
+            if(curIndex==-1){
+                if(i-start==1||num[i]>num[i-2]){
+                        curMax=num[i];
+                        curIndex=i-1;
+
+                }else{
+                    curIndex=i;
+                }
+            }else{
+                res=Math.max(res,i-start);
+                System.out.println(res);
+                while(curIndex!=-1){
+                    start++;
+                    if(start>curIndex)
+                        curIndex=-1;
+                }
+                if(i-start==1||num[i]>num[i-2]){
+                    curMax=num[i];
+                    curIndex=i-1;
+
+                }else{
+                    curIndex=i;
+                }
+            }
+        }
+        res=Math.max(res,num.length-start);
+        return res;
+    }
+
     public static void main(String[] args){
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
         int[] num=new int[n];
         for(int i=0;i<n;i++)
             num[i]=s.nextInt();
-        int start=0;
-        int curMax=num[0];
-        int sIndex=-1;
-        for(int i=1;i<n;i++){
-            if(num[i]>curMax)
-                continue;
-            else if(sIndex==-1){
-                curMax=Math.min(num[i],curMax);
-            }
-        }
+        int res=getNum(num);
+        System.out.println(res);
     }
 }
