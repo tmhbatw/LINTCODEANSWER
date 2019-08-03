bc1175dbcd7e28907854e4b1f2ea5121799caeba
tmhbatw <1060226998@qq.com>
2019-08-03 16:36:47 +0800

    LINTCODE answer                  更新了LINTCODE第660题的解答

---
+++ src/LINTCODE7/LINTCODE660.java
@@ -0,0 +1,24 @@
+package LINTCODE7;
+
+public class LINTCODE660 {
+
+    /*Description
+    * 接口：int read4(char * buf)一次从文件中读取 4 个字符。
+    * 返回值是实际读取的字符数。 例如，如果文件中只剩下 3 个字符，则返回 3。
+    * 通过使用read4 接口，实现从文件读取 n 个字符的函数int read(char * buf，int n)。
+    * */
+    /* The read4 API is defined in the parent class Reader4.
+      int read4(char[] buf); */
+
+    int read4(char[] buf){
+        return 0;
+    }
+
+    char[] buffer=new char[4];
+    int last=0;
+    public int read(char[] buf, int n) {
+        return 0;
+
+        // Write your code here
+    }
+}
