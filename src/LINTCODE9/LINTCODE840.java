cb8a2f19e553d5b0177b91f61480ec9c66e3f66f
tmhbatw <1060226998@qq.com>
2019-08-09 19:13:42 +0800

    LINTCODE answer                  更新了LINTCODE第840题的解答

--- src/LINTCODE9/LINTCODE840.java
+++ src/LINTCODE9/LINTCODE840.java
@@ -7,4 +7,28 @@ public class LINTCODE840 {
     * sumRange(l, r) 返回数组下标在
     * [l,r]区间的元素的和
     * */
+
+    /*Solution
+    * 可以建立一颗数组树
+    * */
+
+    class NumArray {
+
+        int[] nums;
+
+        public NumArray(int[] nums) {
+            this.nums=nums;
+        }
+
+        public void update(int i, int val) {
+            nums[i]=val;
+        }
+
+        public int sumRange(int i, int j) {
+            int res=0;
+            for(int ii=i;ii<=j;ii++)
+                res+=nums[ii];
+            return res;
+        }
+    }
 }
