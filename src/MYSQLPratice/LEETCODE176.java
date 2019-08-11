d3f504fa6b6075c6945b7ba558295b4b7e57be7c
tmhbatw <1060226998@qq.com>
2019-08-11 17:03:13 +0800

    更新了与sql有关的题目

---
+++ src/MYSQLPratice/LEETCODE176.java
@@ -0,0 +1,24 @@
+package MYSQLPratice;
+
+public class LEETCODE176 {
+    /*Description
+    * SQL架构
+编写一个 SQL 查询，获取 Employee 表中第二高的薪水（Salary） 。
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+例如上述 Employee 表，SQL查询应该返回 200 作为第二高的薪水。如果不存在第二高的薪水，那么查询应返回 null。
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+    * */
+    /*Solution
+    * select (select distinct Salary from employee order by salary desc limit 1 offset 1) as SecondHighestSalary
+    * */
+}
