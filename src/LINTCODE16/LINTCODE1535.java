c4cae27b417a231c8b32874747dccf82699344b2
tmhbatw <1060226998@qq.com>
2020-01-12 04:51:53 +0800

    LINTCODE answer                  更新了LINTCODE第1535题的解答

---
+++ src/LINTCODE16/LINTCODE1535.java
@@ -0,0 +1,12 @@
+package LINTCODE16;
+
+public class LINTCODE1535 {
+    /*Description
+    *实现函数 ToLowerCase()，该函数接收一个字符串参数 str，并将该字符串中的大写字母转换成小写字母，之后返回新的字符串。
+    * */
+
+    public String toLowerCase(String str) {
+        return str.toLowerCase();
+        // Write your code here
+    }
+}
