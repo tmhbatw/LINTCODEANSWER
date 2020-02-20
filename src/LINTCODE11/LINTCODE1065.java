0de1b7e180c27ea9ba593c0f5a230850d0b3ab9a
tmhbatw <1060226998@qq.com>
2020-02-21 04:53:44 +0800

    更新了LintCode1065题的答案

--- src/LINTCODE11/LINTCODE1065.java
+++ src/LINTCODE11/LINTCODE1065.java
@@ -1,5 +1,8 @@
 package LINTCODE11;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LINTCODE1065 {
     /*Description
     * 实现MyCalendar类来存储您的活动。 如果新添加的活动没有重复，则可以添加。
@@ -8,4 +11,34 @@ public class LINTCODE1065 {
     * 每次调用MyCalendar.book方法时，如果没有发生重复预定，那么事件可以成功添加到日历，且返回true。 否则，返回false，并且事件不会添加到日历中。
     * 你的类以此方式被调用：MyCalendar cal = new MyCalendar(); MyCalendar.book(start, end);
     * */
+
+    class MyCalendar {
+        List<int[]> list;
+
+        public MyCalendar() {
+            list=new ArrayList<>();
+        }
+
+        public boolean book(int start, int end) {
+            if(list.size()==0){
+                list.add(new int[]{start,end});
+                return true;
+            }
+            for(int i=0;i<list.size();i++){
+                if(end<=list.get(i)[0]){
+                    if(i==0||list.get(i-1)[1]<=start){
+                        list.add(i,new int[]{start,end});
+                        return true;
+                    }else
+                        return false;
+                }
+            }
+            if(list.get(list.size()-1)[1]<=start){
+                list.add(new int[]{start,end});
+                return true;
+            }else
+                return false;
+
+        }
+    }
 }
