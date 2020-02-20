4c772e3a7c47eda4b0dea11efd9ffc5783082b0f
tmhbatw <1060226998@qq.com>
2020-02-21 05:08:57 +0800

    更新了LintCode1181题的答案

--- src/LINTCODE12/LINTCODE1181.java
+++ src/LINTCODE12/LINTCODE1181.java
@@ -2,7 +2,7 @@ package LINTCODE12;
 
 import datastructure.TreeNode;
 
-public class LINTCODE1184 {
+public class LINTCODE1181 {
     /*Description
     * 给定一颗二叉树，您需要计算树的直径长度。 二叉树的直径是树中
     * 任意两个节点之间最长路径的长度。 此路径不一定会通过树根。
