d6a11edd86924d0f540f06e1c0f3d215b32007ad
tmhbatw <1060226998@qq.com>
2020-03-05 21:30:59 +0800

    更新了LintCode1599题的答案

---
+++ src/LINTCODE16/LINTCODE1599.java
@@ -0,0 +1,33 @@
+package LINTCODE16;
+
+public class LINTCODE1599 {
+    /*Description
+    * 在 N * N 的网格中，我们放置了一些与 x，y，z 三轴对齐的 1 * 1 * 1 立方体。
+    * 每个值 v = grid[i][j] 表示 v 个正方体叠放在单元格 (i, j) 上。
+    * 现在，我们查看这些立方体在 xy、yz 和 zx 平面上的投影。
+    * 投影就像影子，将三维形体映射到一个二维平面上。
+    * 在这里，从顶部、前面和侧面看立方体时，我们会看到“影子”。
+    * 返回所有三个投影的总面积。
+    * */
+
+    public int projectionArea(int[][] grid) {
+        int result=0;
+        for(int i=0;i<grid.length;i++){
+            int max=0;
+            for(int j=0;j<grid[0].length;j++){
+                max=Math.max(max,grid[i][j]);
+                result+=grid[i][j]>0?1:0;
+            }
+            result+=max;
+        }
+        for(int j=0;j<grid[0].length;j++){
+            int max=0;
+            for(int i=0;i<grid.length;i++){
+                max=Math.max(grid[i][j],max);
+            }
+            result+=max;
+        }
+        return result;
+        // write your code here
+    }
+}
