e35a963c75dba00c9777715d04b63b4a88bf38ca
tmhbatw <1060226998@qq.com>
2020-03-06 05:41:56 +0800

    更新了LintCode473题的答案

--- src/LINTCODE5/LINTCODE473.java
+++ src/LINTCODE5/LINTCODE473.java
@@ -1,66 +1,64 @@
 package LINTCODE5;
 
-import datastructure.ParentTreeNode;
-
-import java.util.Dictionary;
 import java.util.HashMap;
 
-class WordDictionary {
+public class LINTCODE473 {
+    class WordDictionary {
 
-    /*Description
-    * 设计一个包含下面两个操作的数据结构：addWord(word), search(word)
-    * addWord(word)会在数据结构中添加一个单词。而search(word)则支持普通的单词查询或是只包含.和a-z的简易正则表达式的查询。
-    * 一个 . 可以代表一个任何的字母。
-    * */
+        /*Description
+         * 设计一个包含下面两个操作的数据结构：addWord(word), search(word)
+         * addWord(word)会在数据结构中添加一个单词。而search(word)则支持普通的单词查询或是只包含.和a-z的简易正则表达式的查询。
+         * 一个 . 可以代表一个任何的字母。
+         * */
 
-    /*Solution
-    * 与471题单词树的思路基本相似，只不过判断单词的时候需要在搜查到"."字符时进行机递归处理
-    * */
+        /*Solution
+         * 与471题单词树的思路基本相似，只不过判断单词的时候需要在搜查到"."字符时进行机递归处理
+         * */
 
-    boolean isWord=false;
-    HashMap<Character,WordDictionary> map=new HashMap<>();
-    static boolean exist=false;
+        boolean isWord=false;
+        HashMap<Character,WordDictionary> map=new HashMap<>();
+        boolean exist=false;
 
 
-    public void addWord(String word) {
-        WordDictionary curr=this;
-        for(int i=0;i<word.length();i++){
-            if(!curr.map.containsKey(word.charAt(i)))
-                curr.map.put(word.charAt(i),new WordDictionary());
-            curr=curr.map.get(word.charAt(i));
+        public void addWord(String word) {
+            WordDictionary curr=this;
+            for(int i=0;i<word.length();i++){
+                if(!curr.map.containsKey(word.charAt(i)))
+                    curr.map.put(word.charAt(i),new WordDictionary());
+                curr=curr.map.get(word.charAt(i));
+            }
+            curr.isWord=true;
+            // write your code here
         }
-        curr.isWord=true;
-        // write your code here
-    }
 
-    /*
-     * @param word: A word could contain the dot character '.' to represent any one letter.
-     * @return: if the word is in the data structure.
-     */
-    public boolean search(String word) {
-        exist =false;
-        recursion(this,word,0);
-        return exist;
-        // write your code here
-    }
-
-    public void recursion(WordDictionary dic,String word, int start){
-        if(exist)
-            return;
-        if(start==word.length()&&dic.isWord){
-            exist=true;
-            return;
+        /*
+         * @param word: A word could contain the dot character '.' to represent any one letter.
+         * @return: if the word is in the data structure.
+         */
+        public boolean search(String word) {
+            exist =false;
+            recursion(this,word,0);
+            return exist;
+            // write your code here
         }
-        if(start==word.length())
-            return;
-        if(word.charAt(start)!='.'){
-            if(!dic.map.containsKey(word.charAt(start)))
+
+        public void recursion(WordDictionary dic,String word, int start){
+            if(exist)
                 return;
-            recursion(dic.map.get(word.charAt(start)),word,start+1);
-        }else{
-            for(HashMap.Entry entry:dic.map.entrySet()){
-                recursion((WordDictionary) entry.getValue(),word,start+1);
+            if(start==word.length()&&dic.isWord){
+                exist=true;
+                return;
+            }
+            if(start==word.length())
+                return;
+            if(word.charAt(start)!='.'){
+                if(!dic.map.containsKey(word.charAt(start)))
+                    return;
+                recursion(dic.map.get(word.charAt(start)),word,start+1);
+            }else{
+                for(HashMap.Entry entry:dic.map.entrySet()){
+                    recursion((WordDictionary) entry.getValue(),word,start+1);
+                }
             }
         }
-    }
 }
