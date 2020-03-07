a9f4fe6eb858879b2caf274dbbb3dc6da4a58388
tmhbatw <1060226998@qq.com>
2020-03-08 06:56:25 +0800

    更新了LintCode449题的答案

---
+++ src/LINTCODE5/LINTCODE449.java
@@ -0,0 +1,12 @@
+package LINTCODE5;
+
+public class LINTCODE449 {
+    /*Description
+    * 将字符转换为一个整数。你可以假设字符是ASCII码，也就是说转换后的整数在0~255之间。
+    * */
+
+    public int charToInteger(char character) {
+        return (int)character;
+        // write your code here
+    }
+}
