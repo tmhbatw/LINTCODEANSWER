9a15c8bbf6e2af827a6db37db139177c4507b9cf
tmhbatw <1060226998@qq.com>
2020-03-11 23:09:31 +0800

    更新了LintCode1067题的答案

---
+++ src/LINTCODE11/LINTCODE1067.java
@@ -0,0 +1,18 @@
+package LINTCODE11;
+
+public class LINTCODE1067 {
+    /*Description
+    * 给定具一个数组，编写一个函数将数组拆分为k个连续的“部分”。
+    * 每个部分的长度应尽可能相等：没有两个部分的大小严格大于1。这可能导致某些部分为空。
+    * 元素应按输入列表中的出现顺序排列，位置靠前的部分的长度应始终大于或等于稍后出现的部分。
+    * 返回数组列表，表示形成的数组的部分。
+    * */
+
+    public int[][] splitLinkedListinParts(int[] root, int k) {
+        if(root.length<k){
+            int[][] result=new int[k][1];
+            for(int i=0;i<)
+        }
+        // Write your code here.
+    }
+}
