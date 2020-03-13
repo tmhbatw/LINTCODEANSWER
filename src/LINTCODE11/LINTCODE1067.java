cdb2200a3ec597d1c75e239ac30c968db9230c36
tmhbatw <1060226998@qq.com>
2020-03-13 21:40:41 +0800

    更新了LintCode1067题的答案

--- src/LINTCODE11/LINTCODE1067.java
+++ src/LINTCODE11/LINTCODE1067.java
@@ -1,5 +1,7 @@
 package LINTCODE11;
 
+import java.util.Arrays;
+
 public class LINTCODE1067 {
     /*Description
     * 给定具一个数组，编写一个函数将数组拆分为k个连续的“部分”。
@@ -9,11 +11,33 @@ public class LINTCODE1067 {
     * */
 
     public int[][] splitLinkedListinParts(int[] root, int k) {
+        int[][] result=new int[k][];
+        int length=root.length;
         if(root.length<k){
-            int[][] result=new int[k][1];
-            for(int i=0;i<)
+            for(int i=0;i<root.length;i++)
+                result[i]=new int[]{root[i]};
+            for(int i=root.length;i<k;i++)
+                result[i]=new int[0];
+            return result;
         }
-        return null;
+        int type1Time=length%k;
+        int type1Number=length/k+1;
+        int type2Number=length/k;
+        int index=0;
+        for(int i=0;i<type1Time;i++){
+            result[i]=Arrays.copyOfRange(root,index,index+type1Number);
+            index+=type1Number;
+        }
+        for(int i=type1Time;i<k;i++){
+            result[i]=Arrays.copyOfRange(root,index,index+type2Number);
+            index+=type2Number;
+        }
+        return result;
         // Write your code here.
+    }
+    public static void main(String[] args){
+        int[] cur=new int[]{1,2,3};
+        int[] curr= Arrays.copyOfRange(cur,0,2);
+        System.out.println(Arrays.toString(curr));
     }
 }
