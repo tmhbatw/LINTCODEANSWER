00f4af8ac024858fa3951b931599529640a4f8e6
tmhbatw <1060226998@qq.com>
2020-03-24 23:34:34 +0800

    更新了LintCode1470题的答案

---
+++ src/LINTCODE15/LINTCODE1470.java
@@ -0,0 +1,14 @@
+package LINTCODE15;
+
+public class LINTCODE1470 {
+    /*Description
+    * 现在有一个数组arr。有两个玩家1号和2号轮流从数组中取数，他们只能从数组的两头进行取数，且一次只能取一个。
+    * 两人都采取最优策略。到最后数组中的数被取完后，谁取的数的总和多，就赢得胜利。1号玩家先取。问最后谁将获胜。
+    * 若1号玩家必胜或两人打成平局，返回1，若2号玩家必胜，返回2。
+    * */
+
+    public int theGameOfTakeNumbers(int[] arr) {
+        return 1;
+        // Write your code here
+    }
+}
