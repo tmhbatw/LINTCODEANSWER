93ac62bfbc0629f2345f0cf201a5ab5acf6424c7
tmhbatw <1060226998@qq.com>
2020-09-12 13:22:39 +0800

    更新了LintCode1906题的答案

--- src/LINTCODE20/LINTCODE1906.java
+++ src/LINTCODE20/LINTCODE1906.java
@@ -1,5 +1,7 @@
 package LINTCODE20;
 
+import java.util.Arrays;
+
 public class LINTCODE1906 {
     /*Description
     *给一个矩阵，找出所有比邻居（上下左右，对角也算，不考虑边界就是8个咯）都严格大的点，返回一个mask矩阵。
@@ -11,26 +13,20 @@ public class LINTCODE1906 {
         int row=mask.length;
         int col=mask[0].length;
         int[][] result=new int[row][col];
+        for(int[] cur:result)
+            Arrays.fill(cur,1);
+        int[][] nearPos=new int[][]{{-1,0},{0,1},{1,0},{0,-1}, {-1,-1},{1,1}, {1,-1}};
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 int cur=mask[i][j];
-                if(i>=1&&j>=1&&mask[i-1][j-1]>cur)
-                    continue;
-                if(i>=1&&mask[i-1][j]>cur)
-                    continue;
-                if(i>=1&&j<col-1&&mask[i-1][j+1]>cur)
-                    continue;
-                if(j>=1&&mask[i][j-1]>cur)
-                    continue;
-                if(j<col-1&&mask[i][j+1]>cur)
-                    continue;
-                if(i<row-1&&j>=1&&mask[i+1][j-1]>cur)
-                    continue;
-                if(i<row-1&&mask[i+1][j]>cur)
-                    continue;
-                if(i<row-1&&j<col-1&&mask[i+1][j+1]>cur)
-                    continue;
-                result[i][j]=1;
+                for(int[] curPos:nearPos){
+                    int ii=i+curPos[0];
+                    int jj=j+curPos[1];
+                    if(ii>=0&&ii<row&&jj>=0&&jj<col&&cur<=mask[ii][jj]) {
+                        result[i][j]=0;
+                        break;
+                    }
+                }
             }
         }
         return result;
