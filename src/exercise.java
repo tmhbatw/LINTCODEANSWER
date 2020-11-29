507b0035e76f46eb8a4436feccd393dbec0b5bba
tmhbatw <1060226998@qq.com>
2020-11-29 15:39:24 +0800

    更新了

--- src/exercise.java
+++ src/exercise.java
@@ -1,5 +1,12 @@
+
+
+
 public class exercise {
     public static void main(String[] args){
-        System.out.println("hello world!");
+        int sum=0;
+        for(int i=0;i<10000;i++){
+            sum+=i;
+        }
+        System.out.println("the sum of the first 100 numbers is"+" "+sum);
     }
 }
