78696aa97c9969c1539fc324907c311b2a2f7205
tmhbatw <1060226998@qq.com>
2021-04-14 12:14:32 +0800

    更新了LINTCODE第1210题的答案

---
+++ src/LINTCODE13/LINTCODE1210.java
@@ -0,0 +1,53 @@
+package LINTCODE13;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class LINTCODE1210 {
+    /*Description
+    * 给定一个整数数组，找到所有不同的可能的升序子序列，一个升序子序列的长度至少应为2。
+    * */
+
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        int length=nums.length;
+        List<List<Integer>>[] result=new List[nums.length];
+        for(int i=length-1;i>=0;i--){
+            result[i]=new ArrayList<>();
+            for(int j=length-1;j>i;j--){
+                if(nums[i]<=nums[j]){
+                    List<Integer> list=new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[j]);
+                    result[i].add(list);
+                    for(List<Integer> cur:result[j]){
+                        List<Integer> curr=new ArrayList<>();
+                        curr.add(nums[i]);
+                        curr.addAll(cur);
+                        result[i].add(curr);
+                    }
+                }
+            }
+        }
+        List<List<Integer>> res=new ArrayList<>();
+        for(List<List<Integer>> cur:result)
+            res.addAll(cur);
+        Collections.sort(res,(o1,o2)->{
+            for(int i1=0,i2=0;i1<o1.size()&&i2<o2.size();i1++,i2++){
+                if(o1.get(i1)==o2.get(i2))
+                    continue;
+                return o1.get(i1)-o2.get(i2);
+            }
+            return o1.size()-o2.size();
+        });
+        for(int i=res.size()-1;i>=0;i--){
+            List<Integer> cur=res.get(i);
+            while(i>=1&&res.get(i-1).equals(cur)){
+                res.remove(--i);
+            }
+        }
+        return res;
+        // Write your code here
+    }
+}
