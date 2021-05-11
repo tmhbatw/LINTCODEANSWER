a5e9d6a981ce6db5d10c7a38200b41347f9ed87d
tmhbatw <1060226998@qq.com>
2021-05-11 14:39:20 +0800

    更新了LINTCODE10题解

--- src/LINTCODE1/LINTCODE10.java
+++ src/LINTCODE1/LINTCODE10.java
@@ -13,11 +13,42 @@ public class LINTCODE10 {
     * （PS：递归回溯算法适用于大部分题型）
     * */
 
+    boolean[] reached;
+    char[] s;
+    List<String> result;
+    public List<String> stringPermutation2(String str) {
+        s=str.toCharArray();
+        Arrays.sort(s);
+        reached=new boolean[str.length()];
+        result=new ArrayList<>();
+        dfs(0,"");
+        return result;
+        // write your code here
+    }
 
+    private void dfs(int n,String cur){
+        int length=s.length;
+        if(n==length){
+            result.add(cur);
+            return;
+        }
+        for(int i=0;i<length;i++){
+            if(reached[i])
+                continue;
+            cur+=s[i];
+            int start=i;
+            while(i<length-1&&s[i+1]==s[i])
+                i++;
+            reached[start]=true;
+            dfs(n+1,cur);
+            reached[start]=false;
+            cur=cur.substring(0,cur.length()-1);
+        }
+    }
 
 
     //一年前的方法
-    public List<String> stringPermutation2(String str) {
+/*    public List<String> stringPermutation2(String str) {
         Set<String> result=new HashSet<>();
         int length=str.length();
         boolean[] reached=new boolean[length];
@@ -46,5 +77,5 @@ public class LINTCODE10 {
     }
     private String removeChar(String str,int i){
         return str.substring(0,i)+str.substring(i+1,str.length());
-    }
+    }*/
 }
