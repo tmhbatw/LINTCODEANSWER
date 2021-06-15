a05566dd7bb6c020c360e8fa6cf67b0e73376cdc
liuruihong <liuruihong@bytedance.com>
2021-06-15 13:44:56 +0800

    更新了第581题的答案
    Change-Id: I28405e1851d2041cafd7dff7dda7033c3457d59b

---
+++ src/LINTCODE8/LINTCODE751.java
@@ -0,0 +1,15 @@
+package LINTCODE8;
+
+public class LINTCODE751 {
+    /*Description
+    * 在一条数轴上，有n个城市，编号从0 ~ n – 1 , 约翰打算在这n个城市做点生意，他对Armani的一批货物感兴趣，每个城市对于这批货物都有一个价格prices[i]。
+    * 对于城市x,约翰可从城市编号为[x - k, x + k]购买货物，然后卖到城市x,问约翰在每个城市最多能赚到多少钱？
+    * prices.length 范围为[2, 100000], k <= 100000。
+    * */
+
+    public int[] business(int[] A, int k) {
+        int length=A.length;
+        int[] result=new int[length];
+        //
+    }
+}
---
+++ src/LINTCODE9/LINTCODE860.java
@@ -0,0 +1,13 @@
+package LINTCODE9;
+
+public class LINTCODE860 {
+    /*Description
+    * 给定一个由0和1组成的非空的二维网格，一个岛屿是指四个方向（包括横向和纵向）都相连的一组1（1表示陆地）。你可以假设网格的四个边缘都被水包围。
+    * 找出所有不同的岛屿的个数。如果一个岛屿与另一个岛屿形状相同（不考虑旋转和翻折），我们认为这两个岛屿是相同的。
+    * */
+
+    public int numberofDistinctIslands(int[][] grid) {
+        return 0;
+        // write your code here
+    }
+}
