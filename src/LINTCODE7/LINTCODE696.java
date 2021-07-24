72058f59e67d1807673002ed5ccfd4b5c930de80
tmhbatw <1060226998@qq.com>
2021-07-25 00:38:00 +0800

    更新了LINTCODE696题的答案

--- src/LINTCODE7/LINTCODE696.java
+++ src/LINTCODE7/LINTCODE696.java
@@ -1,41 +1,36 @@
 package LINTCODE7;
 
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class LINTCODE696 {
 
     /*Description
-    * 这里有 n 门不同的线上课程, 编号为 1 到 n. 每一节课都有持续时间(课程长度) t 和 在第 d 天关闭. 课程应连续持续 t 天，必须在第d天或之前完成, 你将从第一天开始.
+    * 这里有 n 门不同的线上课程, 编号为 1 到 n. 每一节课都有持续时间(课程长度) t 和 在第 d 天关闭.
+    * \课程应连续持续 t 天，必须在第d天或之前完成, 你将从第一天开始.
     * 给出 n 门线上课程用 pairs (t, d) 来表示, 你的任务是找到可以上的最大数量的课程数.
     * */
     public int scheduleCourse(int[][] courses) {
-        PriorityQueue<int[]> queue=new PriorityQueue<>(new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if(o1[1]==o2[1])
-                    return o1[0]-o2[0];
-                return o1[1]-o2[1];
-            }
+        PriorityQueue<Integer> q=new PriorityQueue<>(Comparator.reverseOrder());
+        int count =0;
+        int time = 0;
+        Arrays.sort(courses,(o1,o2)->{
+            return o1[1]-o2[1];
         });
-        System.out.println(new Integer(3));
         for(int[] cur:courses){
-            queue.add(cur);
-        }
-        int start=0;
-        int num=0;
-        Queue<int[]> res=new LinkedList<>();
-
-        while(!queue.isEmpty()){
-            int[] cur=queue.poll();
-            if(cur[0]+start<=cur[1]){
-                start+=cur[0];
-                num++;
+            if(cur[0]>cur[1])
+                continue;
+            if(time+cur[0]<=cur[1]){
+                count++;
+                time+=cur[0];
+            }else{
+                if(q.isEmpty()||cur[0]>=q.peek())
+                    continue;
+                int replaceValue = q.poll();
+                time -= replaceValue-cur[0];
             }
+            q.add(cur[0]);
         }
-        return num;
+        return count;
         // write your code here
     }
 }
